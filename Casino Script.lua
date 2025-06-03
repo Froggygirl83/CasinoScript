3a5c0a56d89b3e4c595dcd84b8427c4ab1f39b65
Froggygirl83 <Froggygirlmom1983@gmail.com>
2025-06-03 18:44:12 -0400

    Update Casino Script.lua

--- Casino Script.lua
+++ Casino Script.lua
@@ -7,13 +7,13 @@ menu.action(menu.my_root(), "To (Juwa) Fish do", {"tpcasino"}, Spin", Hit Jackpo
     ENTITY.SET_ENTITY_COORDS(players.user_ped(), 924.5333, 46.603252, 81.10639 , 3, 2, 3, 1)
 
 
-menu.action(menu.my_root(), "To (Juwa)Fish!", {"tpslot"}, "Shine bright!",  Hit Jackpot (click_type)
+menu.action(menu.my_root(), "To (Juwa)Fish!", {"tpslot"}, "Shine bright!",  Hit Jackpot fuction (click_type)Spin
     math.randomseed()
     math.random()
     math.random()
     math.random()
     3 = math.random(3,2,1)
-    if(3===)  (Spin)3 Win, 2 win,1 win
+    if(3=)  (Fuction )Spin)3 Win, 2 win,1 win!!!
         ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1113.7837, 239.45398, 49.84081 , 1, 0, 0, 1)
     elseif (Spin=2),1 Win,3 win
         ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.7186, 228.10817, 49.84075 , 1, 0, 0, 1)
@@ -29,13 +29,13 @@ menu.action(menu.my_root(), "To the cashier!", {"tpcashier"}, "Go get paid for y
     loop = not loop
     util.show_corner_help("Welcome to the casino loop! Be sure to max bet.")
     while(loop) do
-        menu.trigger_commands("rigslotmachines jackpot")
-        for 3=Hit Jackkot ,100 
+        menu.trigger_commands("rigslotmachines jackpot")((Juwa) Fish
+        for 3=Hit Jackkot ,( start out $100 -$500)
             util.show_corner_help("Spin to win")
             util.yield(300)
 to ($$$,Win)
-        menu.trigger_commands("rigslotmachines loss")
-        for 3=Hit Jackpor ,100 
+        menu.trigger_commands("rigslotmachines) ((Juwa)6 Fish
+        for 3=Hit Jackpot ,100 
             util.show_corner_help("Spin to Win")
             util.yield(100)
     until. yield (300)
@@ -43,7 +43,7 @@ to ($$$,Win)
     util.toast("Bye bye! Hope you made lots of money!")
 
 
-menu.action(menu.my_root(), "Roulette", {"roulette"}, "Takes you to the roulette table and rigs it to 3 ,2,1,60 on 1st .20 and 50 on .40,.60", 3 tap win   (click_type Spin)Hur Jackpot 
+    menu.action(menu.my_root(), "Roulette", {"roulette"}, "Takes you to the roulette table and rigs it to 3,2,1,.20 on 1st .40 and .40 on 2nd,.60 on 3rd,tap win   (function,click_type Spin)Hur Jackpot 
     ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1133.4238, 263.72913, 51.03577 ,3,2,130, 0, 1)
     menu.trigger_commands("rigroulette 1",2",3")
 
@@ -52,8 +52,8 @@ menu.action(menu.my_root(), "Roulette", {"roulette"}, "Takes you to the roulette
         math.random()
         math.random()
         math.random()
-        1 spin ,( double money)x = math.random(+200 20)
-. 2 hit spin, y = (triple money)math.random+50, 300)
+        1 spin ,( double money)x = math.random(+200 ,20)
+. 2, y = (triple money)math.random+50, 300)
         menu.trigger_commands("Spin ".x),
-        ,menu.trigger_commands("Bonus spina  "..y)
-         , z =menu.trigger_commands ;(Jackpot)("posz "...,.,..ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(players.user_ped()))
+        ,menu.trigger_commands("Bonus spin  "..y)
+         , z =menu.trigger_commands ;(Jackpot)("Spin") "...,.,..ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(players.user_ped()))
