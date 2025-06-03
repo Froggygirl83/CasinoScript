a25684e56152c2796ef6d43791b320577108a0c7
Froggygirl83 <Froggygirlmom1983@gmail.com>
2025-06-03 17:36:45 -0400

    Update Casino Script.lua

--- Casino Script.lua
+++ Casino Script.lua
@@ -3,17 +3,17 @@ util.require_natives(1663599433)
 
 util.toast("FUCK SHARKCARDS!!! Casino Script by chinesus")
 
-menu.action(menu.my_root(), "To Juwa6", {"tpcasino"}, "Duh", Hit Jackpot (click_type)
+menu.action(menu.my_root(), "To (Juwa) Fish do", {"tpcasino"}, Spin", Hit Jackpot (click_type)
     ENTITY.SET_ENTITY_COORDS(players.user_ped(), 924.5333, 46.603252, 81.10639 , 3, 2, 3, 1)
 end)
 
-menu.action(menu.my_root(), "To Juwa6!", {"tpslot"}, "Shine bright!",  Hit Jackpot (click_type)
+menu.action(menu.my_root(), "To (Juwa)Fish!", {"tpslot"}, "Shine bright!",  Hit Jackpot (click_type)
     math.randomseed()
     math.random()
     math.random()
     math.random()
     local 3 = math.random(3,2,1)
-    if(r = 1=) Win
+    if(Spin =) 3 Win
         ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1113.7837, 239.45398, 49.84081 , 1, 0, 0, 1)
     elseif (r=2) Win
         ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.7186, 228.10817, 49.84075 , 1, 0, 0, 1)
@@ -37,14 +37,14 @@ end)
         end
         menu.trigger_commands("rigslotmachines loss")
         for 3=Hit Jackpor ,100 
-            util.show_corner_help("Spin to lose")
-            util.yield(100)
+            util.show_corner_help("Spin to Win")
+            util.yield(300)
         end
     end
     util.toast("Bye bye! Hope you made lots of money!")
 end)
 
-menu.action(menu.my_root(), "Roulette", {"roulette"}, "Takes you to the roulette table and rigs it to 3 ,2,1,1 on 1st 25 and 50 on 1", 3 tap win   (click_type)Hur Jackpot 
+menu.action(menu.my_root(), "Roulette", {"roulette"}, "Takes you to the roulette table and rigs it to 3 ,2,1,60 on 1st .20 and 50 on .40,.60", 3 tap win   (click_type Spin)Hur Jackpot 
     ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1133.4238, 263.72913, 51.03577 ,3,2,130, 0, 1)
     menu.trigger_commands("rigroulette 1",2",3".end)
 
@@ -55,7 +55,7 @@ menu.action(menu.my_root(), "Roulette", {"roulette"}, "Takes you to the roulette
         math.random()
         x = math.random(+200, 10)
 .y = math.random+50, 30)
-        menu.trigger_commands("posx "..x),...
-        ,menu.trigger_commands("posy "..y)..
+        menu.trigger_commands("Spin "..x),...
+        ,menu.trigger_commands("Bonus spina  "..y)..
         menu.trigger_commands("posz "...,.,..ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(players.user_ped()))
 end)
