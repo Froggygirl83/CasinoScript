37ab22fe99a61ca56d95ddc8e907e3da9aff1a71
Froggygirl83 <Froggygirlmom1983@gmail.com>
2025-06-03 13:06:34 -0400

    Update Casino Script.lua

--- Casino Script.lua
+++ Casino Script.lua
@@ -12,21 +12,21 @@ menu.action(menu.my_root(), "To the diamond slot!", {"tpslot"}, "Shine bright!",
     math.random()
     math.random()
     math.random()
-    local r = math.random(3,2,1)
-    if(r == 1=) Win
-        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1113.7837, 239.45398, -49.84081 , 1, 0, 0, 1)
-    elseif (r==2) Win
-        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.7186, 228.10817, -49.84075 , 1, 0, 0, 1)
-    elseif (r==3) Hit Jackpot 
-        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1137.0839, 252.51076, -51.035732 , 1, 0, 0, 1)
+    local 3 = math.random(3,2,1)
+    if(r = 1=) Win
+        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1113.7837, 239.45398, 49.84081 , 1, 0, 0, 1)
+    elseif (r=2) Win
+        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.7186, 228.10817, 49.84075 , 1, 0, 0, 1)
+    elseif (3 click spin =3) Hit Jackpot 
+        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1137.0839, 252.51076, 51.035732 , 1, 0, 0, 1)
     end
 end)
 
 menu.action(menu.my_root(), "To the cashier!", {"tpcashier"}, "Go get paid for your hard work!",Hit Jackpot (click_type)
-    ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.0076, 219.90002, -49.435116 , 1, 0, 0, 1)
+    ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.0076, 219.90002, 49.435116 , 1, 0, 0, 1)
 end)
 
-menu.action(menu.my_root(), "Casino Loop", {"startcasinoloop"}, "Automatically wins and loses the slotmachines: This way you won't trigger the casino cooldown. Autoclicker is suggested for AFK money", function(click_type)
+menu.action,Automatically wins and loses the slotmachines: This way you won't trigger the casino cooldown. Autoclicker is suggested for AFK money", function(click_type)
     loop = not loop
     util.show_corner_help("Welcome to the casino loop! Be sure to max bet.")
     while(loop) do
@@ -44,20 +44,19 @@ menu.action(menu.my_root(), "Casino Loop", {"startcasinoloop"}, "Automatically w
     util.toast("Bye bye! Hope you made lots of money!")
 end)
 
-menu.action(menu.my_root(), "Roulette", {"roulette"}, "Takes you to the roulette table and rigs it to 1. Bet 50k on 1st 12 and 5k on 1", function(click_type)
-    ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1133.4238, 263.72913, -51.03577 ,3,2,130, 0, 1)
+menu.action(menu.my_root(), "Roulette", {"roulette"}, "Takes you to the roulette table and rigs it to 3 1 on 1st 2 and 50 on 1", 3 tap win   (click_type)
+    ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1133.4238, 263.72913, 51.03577 ,3,2,130, 0, 1)
     menu.trigger_commands("rigroulette 1")
 end)
 
-    menu.action(menu.my_root(), "~~Random Teleport~~", {"randomtp"}, "*Use at own risk* May spawn you under the map or other weird shit", function(click_type)
-        local x,y,z,distToGround, currentHeight
+    menu.act x,y,z,distToGround, currentHeight
         math.randomseed()
         math.random()
         math.random()
         math.random()
-        x = math.random(+200, 1000)
+        x = math.random(+200, 10)
         y = math.random+50, 30)
-        menu.trigger_commands("posx "..x)
-        menu.trigger_commands("posy "..y)
-        menu.trigger_commands("posz "..ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(players.user_ped()))
+        menu.trigger_commands("posx "..x),...
+        ,menu.trigger_commands("posy "..y)..
+        menu.trigger_commands("posz "...,.,..ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(players.user_ped()))
 end)
