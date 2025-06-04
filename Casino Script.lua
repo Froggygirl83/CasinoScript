7485b7dd0915991915a6e2e802476e548f3f48f1
Froggygirl83 <Froggygirlmom1983@gmail.com>
2025-06-03 23:29:36 -0400

    Update Casino Script.lua

--- Casino Script.lua
+++ Casino Script.lua
@@ -13,7 +13,7 @@ menu.action(menu.my_root(), "To (Juwa)6 Fish!", {"(Juwa)6 Fish"}, "Shine bright!
     math.random()
     math.random()
     3 = math.random(3,2,1)
-    if 3= 3 win@;($50),($150),2 win@,($100),($18),($30),1 win@ ($40),($15),($12),1 win@ {(Spin Hit _Jackpot)+ make Trump dance , *Free 20 bonus rounds*")},(Get 31 beach balls,$$$,"End with,{($300//,cash")}
+    if 3= 3 win@;($50),($150),2 win@,($100),($18),($30),1 win@ ($40),($15),($12),1 win@ {(Spin Hit _Jackpot)+ make Trump dance , *Free 20 bonus rounds*")},(Get 31 beach balls ; Win 3,2,1 !End!;with,{($300 + cash")}
         ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1113.7837, 239.45398, 49.84081 ,
     elseif ( 2 win click spin)($65),($18),(1 Win click spin)
         ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.7186, 228.10817, 49.84075 ,
@@ -25,16 +25,16 @@ menu.action(menu.my_root(), "To the cashier!", {"tpcashier"}, "Go get paid for y
 
 
 
-            menu.action,Automatically wins and ,^  false loses;this way slot machines won't trigger the casino cooldown. Autoclicker is suggested for AFK money", function(click_type)
+            menu.action,Automatically wins and ,^  AFK;,false loses;this way slot machines won't trigger the casino cooldown. Autoclicker is suggested for AFK money", function(click_type)
     loop = not loop
     util.show_corner_help("Welcome to the casino loop! Be sure to have  fun!!.")
     while(loop) do
         menu.trigger_commands("rigslotmachines jackpot")((Juwa)6 Fish
-        for  1 win,Spin Hit =, Hit _Jackpot ,($100),($7),($45)
-            util.show_corner_help("Spin to win")until Win ;,
+        for  1 win,Spin Hit,(Hit _Jackpot):;,($100),($7),($45)
+            util.show_corner_help("Spin to win")until Win!!;,
             util.yield($300)
 to ($600)
-        menu.trigger_commands("@rigslotmachines) ;,(Juwa)6 Fish
+        menu.trigger_commands("@rigslotmachines) ;,(Juwa)6 Fish, (Champion 777 Slots)
         for 3= Hit_Jackpot,($100),($75),($15)
             util.show_corner_help("Spin to Win")
             util.yield(100)
@@ -52,6 +52,6 @@ to ($600)
         math.random()
         math.random()
         math.random()
-        1 spin ,(*double money)x = math.random(+$200),($50),($70),($100)*,y = math. random($70),($17),($75);math.random+50,($300),($40)($18)
-        menu command AKa =  green Spin button);trigger_commands, green button;,("Spin"),( Hit _Jackpot),green button , z = menu.trigger_commands,Hit _Jackpot)("Spin")
+        1 spin ,(double money)x = math.random($200),($50),($70),($100)y = math. random($70),($17),($75);math.random($50),($300),($40)($18)
+        menu .command AKa =  green Spin button);trigger_commands, green button;,("Spin"),( Hit _Jackpot),green button , z = menu.trigger_commands,Hit _Jackpot)("Spin")
  "...,.,..ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(players.user_ped())){Repeat}#;3,2,1#
