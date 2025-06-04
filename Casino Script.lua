edf7c9114ad794d10b3a66ff8f007828ce59daca
Froggygirl83 <Froggygirlmom1983@gmail.com>
2025-06-03 22:43:23 -0400

    Update Casino Script.lua

--- Casino Script.lua
+++ Casino Script.lua
@@ -3,40 +3,39 @@ util.require_natives(1663599433)
 
 util.toast("FUCK SHARKCARDS!!! Casino Script by chinesus")
 
-menu.action(menu.my_root(), "To (Juwa) Fish ", {"tpcasino"}, "Spin", (Hit _Jackpot) (click_type)
+menu.action(menu.my_root(), "To (Juwa)6 Fish", {"(Juwa)6 "}, "Spin", (Hit _Jackpot) (click_type)
     ENTITY.SET_ENTITY_COORDS(players.user_ped(), 924.5333, 46.603252, 81.10639 , 3, 2, 3, 1)
 
 
-menu.action(menu.my_root(), "To (Juwa)Fish!", {"(Juwa)"}, "Shine bright!",  (Hit_ Jackpot),fuction (click_type);(Hit_Jackpot)
+menu.action(menu.my_root(), "To (Juwa)6 Fish!", {"(Juwa)6 Fish"}, "Shine bright!",  (Hit_ Jackpot),fuction (click_type);(Hit_Jackpot)
     math.randomseed()
     math.rando()
     math.random()
     math.random()
     3 = math.random(3,2,1)
-    if 3= (Spin),3 win;($50),($150),2 (Spin)win;,($100),($18) 1 (Spin),win;,($40),($15);,1 win (Spin Hit _Jackpot)+ make Trump dance;, Free 20 bonus rounds")),(((Get 31 beach balls!!)));;,End with,{($300 cash 
-+ 1 Win)}
-        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1113.7837, 239.45398, 49.84081 , 1, 0, 0, 1)
-    elseif (Spin=2),1 Win,3 win
-        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.7186, 228.10817, 49.84075 , 1, 0, 0, 1)
-    elseif (3 click spin) ,(3) Hit ("Jackpot")
-        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1137.0839, 252.51076, 51.035732 , 1, 0, 0, 1)
+    if 3= 3 win@;($50),($150),2 win@,($100),($18)& ($30),1 win@ ($40),($15),& ($12),1 win@ {(Spin Hit _Jackpot)+ make Trump dance , Free 20 bonus rounds")},(Get 31 beach balls,$$$,"End with,{($300 cash+1 Win")}
+        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1113.7837, 239.45398, 49.84081 ,
+    elseif (Spin=2)(2),1 Win,3 win
+        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.7186, 228.10817, 49.84075 ,
+    elseif (3 win click spin) ,(3) ("Hit_Jackpot")
+        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1137.0839, 252.51076, 51.035732 ,
 
 menu.action(menu.my_root(), "To the cashier!", {"tpcashier"}, "Go get paid for your hard work!",Hit Jackpot (click_type)
     ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.0076, 219.90002, 49.435116 
 
 
 
-            menu.action,Automatically wins.and. loses the slotmachines: This way you won't trigger the casino cooldown. Autoclicker is suggested for AFK money", function(click_type)
+            menu.action,Automatically wins and rarely loses /- (Hit _ Jackpot),this way slot machines : This way you won't trigger the casino cooldown. Autoclicker is suggested for AFK money", function(click_type)
     loop = not loop
-    util.show_corner_help("Welcome to the casino loop! Be sure to max bet.")
+    util.show_corner_help("Welcome to the casino loop! Be sure to hqvw fun!!.")
     while(loop) do
         menu.trigger_commands("rigslotmachines jackpot")((Juwa)6 Fish
-        for Spin Hit =,Jackpot ,($100)
-            util.show_corner_help("Spin to win")
+        for  1 win,Spin Hit =, Hit _Jackpot ,($100),($7),($45)
+            util.show_corner_help("Spin to win")until Win ;,
             util.yield($300)
-to ($$$,Win)
+to ($600)
         menu.trigger_commands("rigslotmachines) ((Juwa)6 Fish
-        for 3= Hit Jackpot ,($100)
+        for 3= Hit_Jackpot,($100),($75),($15)......
             util.show_corner_help("Spin to Win")
             util.yield(100)
     until. yield ($300)
