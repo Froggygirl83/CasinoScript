1ef22b21315e3de7fc1219462d34f7804154d089
Froggygirl83 <Froggygirlmom1983@gmail.com>
2025-06-04 19:17:51 -0400

    Update Casino Script.lua

--- Casino Script.lua
+++ Casino Script.lua
@@ -15,12 +15,7 @@ menu.action(menu.my_root(), "To (Juwa)6 Fish!", {"(Juwa)6 Fish"}, "Shine bright!
     math.random()
     math.random()
     (3)math.random(3,2,1)
-    if (3),($50),($150),(2),($100),($18),($30),(1),($40),($15),($12),;(1),Spin Hit _Jackpot) & make Trump dance , & Free 20 bonus rounds!!Get 31 beach balls ; Win 3,2,1 !End with Win!!"
-        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1113.7837, 239.45398, 49.84081 ,
-    elseif ( 2 win click spin)($65),($18),(1 Win click spin)
-        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.7186, 228.10817, 49.84075 ,
-    elseif (3 win click ;spin) ,(3) Hit_Jackpot,(click_type)
-        ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1137.0839, 252.51076, 51.035732 ,
+    if (3),($50),($150),(2),($100),($18),($30),(1),($40),($15),($12),;(1),Spin Hit _Jackpot) & make Trump dance , & Free 20 bonus rounds!!Get 31 beach balls ; Win 3,2,1 !End 
 
 menu.action(menu.my_root(), "To the cashier!", {"tpcashier"}, "Go get paid for your hard work!",Hit _Jackpot (click_type)
     ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.0076, 219.90002, 49.435116 
@@ -30,9 +25,6 @@ menu.action(menu.my_root(), "To the cashier!", {"tpcashier"}, "Go get paid for y
             menu.action,Automatically wins!!;Autoclicker is suggested for AFK money", function(click_type)
     util.show_corner_help("Welcome to the casino loop! Be sure to bet!!;,while(loop) do
         menu.trigger_commands("rigslotmachines jackpot")((Juwa)6 Fish,(Champion 777)
-        for  1 win,Spin Hit,(Hit _Jackpot);,($100),($7),($45)
-            util.show_corner_help("Spin to win")until Win!!;,
-            util.yield;
 to win!!
         menu.trigger_commands("@rigslotmachines) ;,(Juwa)6 Fish, (Champion 777 Slots)
         for 3 win Hit_Jackpot,($100),($75),($15)
