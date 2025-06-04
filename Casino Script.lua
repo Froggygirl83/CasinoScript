0cc8950adbc399a2ad6aa2144749bce014c1e5a9
Froggygirl83 <Froggygirlmom1983@gmail.com>
2025-06-03 23:03:56 -0400

    Update Casino Script.lua

--- Casino Script.lua
+++ Casino Script.lua
@@ -3,8 +3,8 @@ util.require_natives(1663599433)
 
 util.toast("FUCK SHARKCARDS!!! Casino Script by chinesus")
 
-menu.action(menu.my_root(), "To (Juwa)6 Fish", {"(Juwa)6 "}, "Spin", (Hit _Jackpot) (click_type)
-    ENTITY.SET_ENTITY_COORDS(players.user_ped(), 924.5333, 46.603252, 81.10639 , 3, 2, 3, 1)
+menu.action(menu.my_root(), "To (Juwa)6 Fish", {"(Juwa)6 Fish"}, "Spin", (Hit _Jackpot) (click_type)
+    ENTITY.SET_ENTITY_COORDS(players.user_ped(), 924.5333, 46.603252, 81.10639 , 3, 2, 1.
 
 
 menu.action(menu.my_root(), "To (Juwa)6 Fish!", {"(Juwa)6 Fish"}, "Shine bright!",  (Hit_ Jackpot),fuction (click_type);(Hit_Jackpot)
@@ -13,28 +13,28 @@ menu.action(menu.my_root(), "To (Juwa)6 Fish!", {"(Juwa)6 Fish"}, "Shine bright!
     math.random()
     math.random()
     3 = math.random(3,2,1)
-    if 3= 3 win@;($50),($150),2 win@,($100),($18)& ($30),1 win@ ($40),($15),& ($12),1 win@ {(Spin Hit _Jackpot)+ make Trump dance , Free 20 bonus rounds")},(Get 31 beach balls,$$$,"End with,{($300 cash+1 Win")}
+    if 3= 3 win@;($50),($150),2 win@,($100),($18),($30),1 win@ ($40),($15),($12),1 win@ {(Spin Hit _Jackpot)+ make Trump dance , *Free 20 bonus rounds*")},(Get 31 beach balls,$$$,"End with,{($300//;cash+1 Win")}
         ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1113.7837, 239.45398, 49.84081 ,
-    elseif (Spin=2)(2),1 Win,3 win
+    elseif (Spin 2),1 Win,3 win
         ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.7186, 228.10817, 49.84075 ,
     elseif (3 win click spin) ,(3) ("Hit_Jackpot")
         ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1137.0839, 252.51076, 51.035732 ,
 
-menu.action(menu.my_root(), "To the cashier!", {"tpcashier"}, "Go get paid for your hard work!",Hit Jackpot (click_type)
+menu.action(menu.my_root(), "To the cashier!", {"tpcashier"}, "Go get paid for your hard work!",Hit _Jackpot (click_type)
     ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1116.0076, 219.90002, 49.435116 
 
 
 
-            menu.action,Automatically wins and rarely loses /- (Hit _ Jackpot),this way slot machines : This way you won't trigger the casino cooldown. Autoclicker is suggested for AFK money", function(click_type)
+            menu.action,Automatically wins and ,^ loses;this way slot machines won't trigger the casino cooldown. Autoclicker is suggested for AFK money", function(click_type)
     loop = not loop
-    util.show_corner_help("Welcome to the casino loop! Be sure to hqvw fun!!.")
+    util.show_corner_help("Welcome to the casino loop! Be sure to have  fun!!.")
     while(loop) do
         menu.trigger_commands("rigslotmachines jackpot")((Juwa)6 Fish
         for  1 win,Spin Hit =, Hit _Jackpot ,($100),($7),($45)
             util.show_corner_help("Spin to win")until Win ;,
             util.yield($300)
 to ($600)
-        menu.trigger_commands("rigslotmachines) ((Juwa)6 Fish
+        menu.trigger_commands("@rigslotmachines) ;,(Juwa)6 Fish
         for 3= Hit_Jackpot,($100),($75),($15)......
             util.show_corner_help("Spin to Win")
             util.yield(100)
@@ -43,16 +43,15 @@ to ($600)
     util.toast("Bye bye! Hope you made lots of money!")
 
 
-    menu.action(menu.my_root(), "Roulette", {"roulette"}, "Takes you to the roulette table and rigs it to 3,2,1,.20 on 1st .40 , hir $50 and .40 on 2nd,$100 on 3rd,tap win   ,click_type(Spin)Hur ("Jackpot")
+    menu.action(menu.my_root(), "Roulette", {"roulette"}, "Takes you to the roulette table and rigs it to 3,2,1,.20 on 1st .40 2nd,.60 on 3rd ,;click_type(Spin)Hur (" Hit_Jackpot")
     ENTITY.SET_ENTITY_COORDS(players.user_ped(), 1133.4238, 263.72913, 51.03577 ,3,2,130, 0, 1)
     menu.trigger_commands("rigroulette 1",2",3")
 
     menu.act x,y,z,distToGround, currentHeight
-        math.randomseed()($300)
+        math.randomseed()
         math.random()
         math.random()
         math.random()
-        1 spin ,(double money)x = math.random(+$200),($50),($70),($100),
-y = math. random($70),($80)(triple money);math.random+50,($300),($40)($18)
-        home.trigger_commands, green button;,("Spin"),(Jackpot),green button , z = home.trigger_commands,(Jackpot)("Spin")
- "...,.,..ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(players.user_ped())){Repeat}
+        1 spin ,(*double money)x = math.random(+$200),($50),($70),($100)*,y = math. random($70),($17),($75);math.random+50,($300),($40)($18)
+        menu command ;.trigger_commands, green button;,("Spin"),( Hit _Jackpot),green button , z = home.trigger_commands,Hit _Jackpot)("Spin")
+ "...,.,..ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(players.user_ped())){Repeat}#;3,2,1#
